bf6c137d80bb469a4a91d2c9fee82f5a359ed7c2
Tom Kim <ekagud@gmail.com>
2016-03-29 10:14:13 +0900

    Changed test script from es6 to es5
    Travis CI’s Chrome version is to low to support native es6

--- test/spec/simple-cacher.spec.js
+++ test/spec/simple-cacher.spec.js
@@ -1,74 +1,74 @@
-describe('Cacher', () => {
+describe('Cacher', function () {
   'use strict';
 
-  let collection = [
+  var collection = [
     { id: 1, name: 'tom' },
     { id: 2, name: 'jade' },
     { id: 3, name: 'smith' },
   ];
 
-  let collection2 = [
+  var collection2 = [
     { id: 2, name: 'john' },
     { id: 4, name: 'smith' }
   ];
 
-  let collectionMerged = [
+  var collectionMerged = [
     { id: 1, name: 'tom' },
     { id: 2, name: 'john' },
     { id: 3, name: 'smith' },
     { id: 4, name: 'smith' }
   ];
 
-  let collectionMergedByName = [
+  var collectionMergedByName = [
     { id: 1, name: 'tom' },
     { id: 2, name: 'jade' },
     { id: 4, name: 'smith' },
     { id: 2, name: 'john' }
   ];
 
-  let fixtureSingle = { name: 'andy' };
-  let fixtureSingle2 = { name: 'susan' };
+  var fixtureSingle = { name: 'andy' };
+  var fixtureSingle2 = { name: 'susan' };
 
-  beforeEach(() => {
+  beforeEach(function () {
     Cacher.storage = null;
   });
 
   // Cache tests
 
-  it('should cache collection', () => {
+  it('should cache collection', function () {
     Cacher.cache('test', collection);
     expect(Cacher.hit('test')).toEqual(collection);
   });
 
-  it('should overwrite cache collection with existing id', () => {
+  it('should overwrite cache collection with existing id', function () {
     Cacher.cache('test', collection);
     Cacher.cache('test', collection2);
     expect(Cacher.hit('test')).toEqual(collectionMerged);
   });
 
-  it('should cache collection with custom id', () => {
+  it('should cache collection with custom id', function () {
     Cacher.cache('test', collection, 'name');
     expect(Cacher.hit('test')).toEqual(collection);
   });
 
-  it('should cache collection with custom and existing id', () => {
+  it('should cache collection with custom and existing id', function () {
     Cacher.cache('test', collection, 'name');
     Cacher.cache('test', collection2, 'name');
     expect(Cacher.hit('test')).toEqual(collectionMergedByName);
   });
 
-  it('should cache single object', () => {
+  it('should cache single object', function () {
     Cacher.cache('test', fixtureSingle);
     expect(Cacher.hit('test')).toEqual(fixtureSingle);
   });
 
-  it('should overwrite cache single object', () => {
+  it('should overwrite cache single object', function () {
     Cacher.cache('test', fixtureSingle);
     Cacher.cache('test', fixtureSingle2);
     expect(Cacher.hit('test')).toEqual(fixtureSingle2);
   });
 
-  it('should cache single object as collection with id', () => {
+  it('should cache single object as collection with id', function () {
     Cacher.cache('test', { id: 3, name: 'smith' });
     expect(Cacher.hit('test')).toEqual([{ id: 3, name: 'smith'}]);
     expect(Cacher.hit('test', 3)).toEqual({ id: 3, name: 'smith'});
@@ -77,12 +77,12 @@ describe('Cacher', () => {
 
   // Hit tests
 
-  it('should hit by id in collection', () => {
+  it('should hit by id in collection', function () {
     Cacher.cache('test', collection);
     expect(Cacher.hit('test', 2)).toEqual({ id: 2, name: 'jade' });
   });
 
-  it('should hit with multiple ids', () => {
+  it('should hit with multiple ids', function () {
     Cacher.cache('test', collection);
     expect(Cacher.hit('test', [1,2,999])).toEqual([
       { id: 1, name: 'tom' },
@@ -90,50 +90,50 @@ describe('Cacher', () => {
     ]);
   });
 
-  it('should force cache single object with id attribute', () => {
+  it('should force cache single object with id attribute', function () {
     Cacher.cache('test', collection[0], null);
     expect(Cacher.hit('test')).toEqual(collection[0]);
   });
 
-  it('should cache array as single object with null id', () => {
+  it('should cache array as single object with null id', function () {
     Cacher.cache('test', collection, null);
     expect(Cacher.hit('test')).toEqual(collection);
   });
 
-  it('should miss by non-existing id', () => {
+  it('should miss by non-existing id', function () {
     Cacher.cache('test', collection);
     expect(Cacher.hit('test', 999)).toBe(null);
   });
 
 
-  // Delete tests
+  // Devare tests
 
-  it('should delete by id in collection', () => {
+  it('should devare by id in collection', function () {
     Cacher.cache('test', collection);
-    Cacher.delete('test', 2);
+    Cacher.devare('test', 2);
     expect(Cacher.hit('test')).toEqual([
       { id: 1, name: 'tom' },
       { id: 3, name: 'smith' }
     ]);
   });
 
-  it('should delete by ids in collection', () => {
+  it('should devare by ids in collection', function () {
     Cacher.cache('test', collection);
-    Cacher.delete('test', [2, 3]);
+    Cacher.devare('test', [2, 3]);
     expect(Cacher.hit('test')).toEqual([
       { id: 1, name: 'tom' }
     ]);
   });
 
-  it('should delete all in collection', () => {
+  it('should devare all in collection', function () {
     Cacher.cache('test', collection);
-    Cacher.delete('test');
+    Cacher.devare('test');
     expect(Cacher.hit('test')).toEqual([]);
   });
 
-  it('should delete single object', () => {
+  it('should devare single object', function () {
     Cacher.cache('test', fixtureSingle);
-    Cacher.delete('test');
+    Cacher.devare('test');
     expect(Cacher.hit('test')).toBe(null);
   });
 });
